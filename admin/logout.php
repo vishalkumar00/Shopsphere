82c0ecfb17b598afc3517d1a7675d884beaea4d7
krunalChhabhaya <krunalchhabhaya7803@gmail.com>
2024-05-29 00:23:45 -0400

    added comments in logout/ file

--- admin/logout.php
+++ admin/logout.php
@@ -1,6 +1,11 @@
 <?php
+// session started if not then redirect back to index (login)
 session_start();
+
+// destroys the session
 session_destroy();
+
+// redirect admin to login (index)
 header("Location: index.php");
 exit();
 ?>
