c129a0455d71168a1c07a35adb56f16e0e373a2d
krunalChhabhaya <krunalchhabhaya7803@gmail.com>
2024-05-29 00:26:04 -0400

    added comments in connection file

--- database/conn.php
+++ database/conn.php
@@ -4,8 +4,10 @@ $username = "root";
 $password = "";
 $database = "shopsphere";
 
+//storing servername, root and database in a variable
 $conn = new mysqli($servername, $username, $password, $database);
 
+// initializing connection
 if($conn->connect_error) {
     die("connection failed:" .$conn->connect->error);
 }
