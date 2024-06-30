9aea6983b1c851ac14f910a6ca26cc5a173756b9
vishalkumar00 <72116944+vishalkumar00@users.noreply.github.com>
2024-06-29 23:13:16 -0400

    add script

--- seller/reset_password.php
+++ seller/reset_password.php
@@ -102,5 +102,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </main>
 
 <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../assets/js/main.js"></script>
 </body>
 </html>
