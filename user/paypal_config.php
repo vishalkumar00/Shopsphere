2b12d9ced660fe39122599896fc86f050e8589e7
Krunal Chhabhaya <159066597+krunalChhabhaya@users.noreply.github.com>
2024-08-18 19:53:25 -0400

    Update paypal_config.php

--- user/paypal_config.php
+++ user/paypal_config.php
@@ -5,8 +5,8 @@ use PayPalCheckoutSdk\Core\PayPalHttpClient;
 use PayPalCheckoutSdk\Core\SandboxEnvironment;
 
 // Set up your PayPal credentials
-$clientId = 'AfFnIfADz3MFxoYQCtrbGSRh57UBZnh68215Bx__mZNKN63dhapnnlEKkPK4hbWFSawNS8oKxZmneZ0f';
-$clientSecret = 'EJwJotMmhZ7f9PYnUlmqyemCc_Mg81kYl4jELQTHl-Ob73rfIUuC07Ovu3NuoLUphEt9_tfLBPMPCmrd';
+$clientId = '';
+$clientSecret = '';
 
 $environment = new SandboxEnvironment($clientId, $clientSecret);
 $client = new PayPalHttpClient($environment);
